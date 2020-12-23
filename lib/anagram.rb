46c40e49af61ba1064c4ad147c9d9c421290ae61
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-22 23:45:32 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,54 @@
 # Your code goes here!
+
+
+class Anagram
+    def initialize(word)
+        @word = word
+        
+    end
+
+    attr_accessor :word
+
+    def match (string_anagram)
+        @list_of_poss_anagrams = string_anagram
+        anagram_list = []
+        @list_of_poss_anagrams.each do |anagram|
+            if @word.split('').sort.join('') == anagram.split('').sort.join('')
+                anagram_list << anagram
+            end 
+        end
+        return anagram_list
+        # puts @anagram_list
+    end
+end
+
+diaper = Anagram.new('diaper')
+puts diaper.word
+puts diaper.match(["helooo", "perdia"])
+
+# hello world zombies pants dipper
+
+
+
+
+
+
+
+
+# if anagram.length != @word.length
+#     return @anagram_list
+#     # return false
+# elsif @word.split('').sort.join('') == anagram.split('').sort.join('')
+#     @anagram_list << anagram
+
+#     # @word.split('').sort.join('') == anagram.split('').sort.join('') ? @anagram_list << anagram : @anagram_list
+
+#     # regex_one = /@word.split('').sort.join('')/
+#     # puts regex_one
+#     # # @word.split('').sort.each_with_index do |letter, index|
+#     # @anagram_list = true
+        
+#     #end 
+#     # == anagram.split('').sort
+#     # @anagram_list << anagram
+# end
